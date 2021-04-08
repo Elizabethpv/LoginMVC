fe3749f2148fe005f05568755aad46c405903750
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-04-08 22:47:25 +0530

    Add project files.

---
+++ LoginMVC/Controllers/PersonDetailsController.cs
@@ -0,0 +1,23 @@
+﻿using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class PersonDetailsController : Controller
+    {
+        // GET: PersonDetails
+        public ActionResult PersonData()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult View(Person person)
+        {
+            return View("About",person);
+        }
+    }
+}
---
+++ LoginMVC/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginMVC.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
